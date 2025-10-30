d96b3249d03fc68a1bac2f8ac3a5cbdbab17602a
webdv <abykurian555@gmail.com>
2025-10-30 15:23:59 +0530

    Add SumOfIntegers class to calculate sum

---
+++ SumOfIntegers.java
@@ -0,0 +1,8 @@
+public class SumOfIntegers {
+    public static void main(String[] args) {
+        Integer a = 25; // Autoboxing
+        Integer b = 35;
+        int sum = a + b; // Unboxing
+        System.out.println("Sum of Integers: " + sum);
+    }
+}
